e9d0b552334d59e19c6eec2767752ba42eaabf10
lamatmed <lamat032025@gmail.com>
2025-03-26 15:25:32 +0100

    Correction des warnings ESLint et optimisation useEffect

--- config/db.js
+++ config/db.js
@@ -1,25 +1,30 @@
+import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 
-let cached = global.mongoose
+// Charger le fichier .env
+dotenv.config();
+
+let cached = global.mongoose;
 
 if (!cached) {
     cached = global.mongoose = { conn: null, promise: null };
 }
+
 async function connectDB() {
-
-
+    console.log("MongoDB URI:", process.env.MONGODB_URI); // Vérifiez si l'URI est correcte
     if (cached.conn) {
-        return cached.conn
+        return cached.conn;
     }
+
     if (!cached.promise) {
         const opts = { bufferCommands: false };
-
         cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/quickcart`, opts).then(mongoose => {
-            return mongoose
+            return mongoose;
         });
     }
+
     cached.conn = await cached.promise;
-    return cached.conn
+    return cached.conn;
 }
 
 export default connectDB;
